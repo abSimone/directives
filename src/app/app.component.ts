47f7ca43318bee4a6815e97b69eacfb4aaf0f75a
abSimone <simone.angeloni.bei@gmail.com>
2024-01-17 11:58:46 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+
+}
